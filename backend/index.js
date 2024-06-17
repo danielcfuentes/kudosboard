c30c9012d7b09c772c49e24ffdd747a493065cbc
Daniel Camilo Fuentes <dcfuentes@meta.com>
2024-06-17 13:02:31 -0700

    set up the backend template

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+// node index.js   this is to run the server
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my app!");
+});
